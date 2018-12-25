8fec92f7e4da3353ec12acb02e2125782716b9d1
RyosukeCla <ryosukeclarinet@gmail.com>
2018-12-26 04:00:47 +0900

    refactor base

--- src/parser/base.rs
+++ src/parser/base.rs
@@ -2,44 +2,57 @@ use std::fmt;
 use std::fmt::Debug;
 
 #[derive(Debug, Clone)]
-pub struct State<K: Clone> {
+pub struct State<T: Clone> {
   pub success: bool,
-  pub node: Option<Node<K>>,
+  pub node: Option<Node<T>>,
   pub position: usize,
 }
 
 #[derive(Debug, Clone)]
-pub struct Node<K: Clone> {
-  pub value: Option<String>,
-  pub children: Option<Vec<Node<K>>>,
-  pub kind: Option<K>,
+pub struct Node<T: Clone> {
+  pub value: Type<T>,
+  pub kind: Option<String>,
 }
 
-pub trait Parser<K: Clone> {
-  fn parse(&self, target: &str, position: usize) -> State<K>;
-  fn box_clone(&self) -> Box<Parser<K>>;
+#[derive(Clone, Debug)]
+pub enum Type<T: Clone> {
+  Str(String),
+  Char(char),
+  Isize(isize),
+  Usize(usize),
+  U8(u8),
+  U16(u16),
+  U32(u32),
+  U64(u64),
+  U128(u128),
+  I16(i16),
+  I32(i32),
+  I64(i64),
+  I128(i128),
+  F32(f32),
+  F64(f64),
+  Bool(bool),
+  Val(T),
+  Arr(Vec<Node<T>>),
 }
 
-fn recursive_fmt<K: Clone + Debug>(node: &Node<K>) -> String {
+pub trait Parser<T: Clone> {
+  fn parse(&self, target: &str, position: usize) -> State<T>;
+  fn box_clone(&self) -> Box<Parser<T>>;
+}
+
+fn recursive_fmt<T: Clone + Debug>(node: &Node<T>) -> String {
   let mut res = "".to_string();
 
   match &node.kind {
     Some(kind) => {
-      res.push_str(format!("{:?} ", kind).as_str());
+      res.push_str(format!("{} ", kind).as_str());
     }
     None => {}
   }
 
   match &node.value {
-    Some(value) => {
-      res.push_str(value.clone().as_str());
-      return res;
-    }
-    None => {}
-  }
-
-  match &node.children {
-    Some(children) => {
+    Type::Arr(children) => {
       res.push_str("[");
 
       for child in children {
@@ -52,15 +65,18 @@ fn recursive_fmt<K: Clone + Debug>(node: &Node<K>) -> String {
 
       res.push_str("]");
 
-      return res;
+      res
     }
-    None => {}
-  }
+    _ => {
+      let val = format!("{:?}", node.value.clone());
+      res.push_str(val.as_str());
 
-  res
+      res
+    }
+  }
 }
 
-impl<K: Clone + Debug> fmt::Display for Node<K> {
+impl<T: Clone + Debug> fmt::Display for Node<T> {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     let res = recursive_fmt(self);
     write!(f, "{}", res)
