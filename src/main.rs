2c7cad9bdf362c31ead68650f4bdc1c13a4d6102
RyosukeCla <ryosukeclarinet@gmail.com>
2018-12-25 21:35:55 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod parser;
 use crate::parser::{
-    parse, Char, Choice, ExtractMap, FlattenMap, Kind, Lazy, Many, RegExp, Seq, Token, WrapMap,
+    parse, Char, Choice, ExtractMap, FlattenMap, Kind, Lazy, Many, RegExp, Seq, Token, UnwrapMap,
+    WrapMap,
 };
 
 #[derive(Clone, Debug)]
@@ -10,22 +11,39 @@ enum Symbol {
     Expr,
 }
 
-pub fn main() {
-    let num = Kind(&RegExp(r"([1-9][0-9]*|[0-9])"), Symbol::Num);
+fn expression_example() {
+    let spaces = Many(&Token(" "));
+    let num = Kind(
+        &UnwrapMap(&ExtractMap(
+            &Seq(&spaces)
+                .and(&RegExp(r"([1-9][0-9]*|[0-9])"))
+                .and(&spaces),
+            1,
+        )),
+        Symbol::Num,
+    );
     let operator = Kind(&Char("+-"), Symbol::Op);
     let parenthesis = Lazy();
     let atom = Choice(&num).or(&parenthesis);
     let expression =
         FlattenMap(&Seq(&WrapMap(&atom)).and(&FlattenMap(&Many(&Seq(&operator).and(&atom)))));
+    let paren_open = Seq(&spaces).and(&Token("(")).and(&spaces);
+    let paren_close = Seq(&spaces).and(&Token(")")).and(&spaces);
 
     parenthesis.set_parser(&FlattenMap(&ExtractMap(
-        &Seq(&Token("(")).and(&expression).and(&Token(")")),
+        &Seq(&paren_open).and(&expression).and(&paren_close),
         1, // extract expression
     )));
 
     let parser = Kind(&expression, Symbol::Expr); // grant Expression label
 
-    let targets = vec!["10+20-(3+1-(4))", "hoge", "1+2-(3+1", "0-3+(((3)))"];
+    let targets = vec![
+        "10+20-(3+1-(4))",
+        "hoge",
+        "1+2-(3+1",
+        "0-3+(((3)))",
+        "1 + 2 + ( 20 + 3 )",
+    ];
 
     for target in targets {
         println!("[In]:\n   {}\n", target);
@@ -38,4 +56,8 @@ pub fn main() {
             }
         }
     }
+}
+
+pub fn main() {
+    expression_example();
 }
