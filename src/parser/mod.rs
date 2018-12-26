aadb4f02ea8d6073807a54faef4780fb761dc46b
RyosukeCla <ryosukeclarinet@gmail.com>
2018-12-27 06:22:01 +0900

    refactor

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -16,23 +16,63 @@ mod type_map;
 mod unwrap_map;
 mod wrap_map;
 
-pub use self::base::{Node, Parser, State, Type};
-pub use self::char::build as Char;
-pub use self::choice::build as Choice;
-pub use self::extract_map::build as ExtractMap;
-pub use self::flatten_map::build as FlattenMap;
-pub use self::kind::build as Kind;
-pub use self::lazy::build as Lazy;
-pub use self::many::build as Many;
-pub use self::map::build as Map;
-pub use self::opt::build as Opt;
-pub use self::regexp::build as RegExp;
-pub use self::seq::build as Seq;
-pub use self::token::build as Token;
-pub use self::trim::build as Trim;
-pub use self::type_map::build as TypeMap;
-pub use self::unwrap_map::build as UnwrapMap;
-pub use self::wrap_map::build as WrapMap;
+use self::base::Parser;
+pub use self::base::{DefaultType, Node, State, Type};
+pub use self::char::build as char;
+pub use self::choice::build as choice;
+pub use self::extract_map::build as extract_map;
+pub use self::flatten_map::build as flatten_map;
+pub use self::kind::build as kind;
+pub use self::lazy::build as lazy;
+pub use self::many::build as many;
+pub use self::map::build as map;
+pub use self::opt::build as opt;
+pub use self::regexp::build as regexp;
+pub use self::seq::build as seq;
+pub use self::token::build as token;
+pub use self::trim::build as trim;
+pub use self::type_map::build as type_map;
+pub use self::unwrap_map::build as unwrap_map;
+pub use self::wrap_map::build as wrap_map;
+
+pub struct ParserCombinator<T: Clone = DefaultType> {
+  pub parser: Box<Parser<T>>,
+}
+
+impl<T: Clone + Sized + 'static> ParserCombinator<T> {
+  pub fn new<P: Parser<T>>(parser: &P) -> Self {
+    ParserCombinator {
+      parser: parser.box_clone(),
+    }
+  }
+
+  pub fn parse(&self, target: &str) -> Result<Node<T>, String> {
+    let result = self.parser.parse(target, 0);
+    if result.success {
+      if result.position == target.len() {
+        if let Some(node) = result.node {
+          return Ok(node);
+        }
+      } else {
+        return Err(format!("Parse Error: failed at {}", 1 + result.position));
+      }
+    }
+
+    Err("Parse Error: failed at 1".to_string())
+  }
+}
+
+impl<T: Clone + 'static> Parser<T> for ParserCombinator<T> {
+  fn box_clone(&self) -> Box<Parser<T>> {
+    Box::new(ParserCombinator {
+      parser: self.parser.box_clone(),
+    })
+  }
+
+  fn parse(&self, target: &str, position: usize) -> State<T> {
+    self.parser.as_ref().parse(target, position)
+  }
+}
 
 pub fn parse<T: Clone, P: Parser<T>>(parser: &P, target: &str) -> Result<Node<T>, String> {
   let result = parser.parse(target, 0);
--- src/parser/trim.rs
+++ src/parser/trim.rs
@@ -1,11 +1,11 @@
 use crate::parser::base::Parser;
 use crate::parser::map;
-use crate::parser::{ExtractMap, Many, Seq};
+use crate::parser::{extract_map, many, seq};
 
 /**
  *  Trim "a" by "-":
  *    "--a-" -> "a"
  */
 pub fn build<T: Clone + 'static, P: Parser<T>, B: Parser<T>>(parser: &P, by: &B) -> map::Map<T> {
-  ExtractMap(&Seq(&Many(by)).and(parser).and(&Many(by)), 1)
+  extract_map(&seq(&many(by)).and(parser).and(&many(by)), 1)
 }
