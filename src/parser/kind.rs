5cedc66641d2c78544286a921ebedfb79324765f
RyosukeCla <ryosukeclarinet@gmail.com>
2018-12-27 06:21:36 +0900

    fix

--- src/parser/kind.rs
+++ src/parser/kind.rs
@@ -4,7 +4,7 @@ use crate::parser::map;
 /**
  *  Grant Kind
  */
-pub fn build<T: Clone + 'static, P: Parser<T>>(parser: &P, name: &'static str) -> map::Map<T> {
+pub fn build<T: Clone, P: Parser<T>>(parser: &P, name: &'static str) -> map::Map<T> {
   map::build(
     parser,
     Box::new(move |node| Node {
